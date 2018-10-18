30532ec95982d3bdc1a487bca3a72d1b3bbedf60
leonardoj6 <l.jun1717@gmail.com>
2018-10-17 21:40:55 -0600

    delete some files

---
+++ build/precache-manifest.ddec6a182f9ddbb6ee16d89025c58107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "dec6811d1ed4c4cc71d1",
+    "url": "/tic-tac-toe/static/js/main.dec6811d.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/tic-tac-toe/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "dec6811d1ed4c4cc71d1",
+    "url": "/tic-tac-toe/static/css/main.d6788c34.chunk.css"
+  },
+  {
+    "revision": "8717261e83c3c1d28d2154e97a3c6016",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
