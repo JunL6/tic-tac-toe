bb69420b70d0dcac85643657684b98de264af282
leonardoj6 <l.jun1717@gmail.com>
2018-10-17 21:50:19 -0600

    Updates

---
+++ precache-manifest.8a2e17f18b26318ac05b2ec106488655.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "1a1bd021e7637a6bf65f",
+    "url": "/tic-tac-toe/static/js/main.1a1bd021.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/tic-tac-toe/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "1a1bd021e7637a6bf65f",
+    "url": "/tic-tac-toe/static/css/main.270e26ac.chunk.css"
+  },
+  {
+    "revision": "7afd100ec5ec6b85f5a7e1fc929f94e1",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
